d5dffba8aabe249898558c348e54fb094c08d1bd
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-05 21:57:03 -0700

    Create wainwright.lua

---
+++ configs/wainwright.lua
@@ -0,0 +1,10 @@
+Config.wainwright = {
+    -- Restrict leveling and shops to configured wainwright jobs.
+    wainwrightOnly = false,
+
+    wainwrightJobs = {
+        wainwright = {
+            minimumGrade = 0,       -- Lowest job grade allowed.
+        },
+    }
+}
