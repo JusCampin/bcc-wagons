4107c2549f7aa523c9eba24a58d1a35968949ea5
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-09 23:03:08 -0700

    Delete quality-stars.sql

--- database/migrations/quality-stars.sql
+++
@@ -1,8 +0,0 @@
--- Run once on databases that stored hunting-wagon quality as 0=poor,
--- 1=good, 2=perfect. New installations already store visible stars (1-3).
-UPDATE `bcc_wagon_hunting_cargo`
-SET `quality` = LEAST(3, `quality` + 1);
-
-ALTER TABLE `bcc_wagon_hunting_cargo`
-    MODIFY `quality` TINYINT UNSIGNED NOT NULL DEFAULT 1
-    COMMENT 'Player-facing carcass quality: 1=poor, 2=good, 3=perfect';
